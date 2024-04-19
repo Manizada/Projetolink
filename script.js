3fac3237162a4e239bb2d7b45a01f7be0be77cab
Geovani <maniyt60@gmail.com>
2024-04-19 15:19:04 -0400

    Change img removed

--- script.js
+++ script.js
@@ -10,11 +10,4 @@ function toggleMode() {
     //substituir a imagem
     //se estiver em light-mode, adicionar a imagem light
     //se estiver sem light mode, manter a imagem normal
-    const imagem = document.querySelector('img')
-    if(html.classList.contains('light-mode')) {
-        //se tiver light-mode, adicionar a imagem light
-        imagem.setAttribute("src", "./assets/miakel-lightmode.jpg")
-    } else {
-        imagem.setAttribute("src", "./assets/miakel.jpg")
-    }
 }
